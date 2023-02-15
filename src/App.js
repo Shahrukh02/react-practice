dbf1cc431c17da49b25969c8ec56b23a16cbd849
Muhammad Shahrukh <shahrukhbest881@gmail.com>
2023-02-15 08:06:59 +0500

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 // import UseEffect from "./components/UseEffectPractice/UseEffect";
 // import Form from "./components/formPractice/Form"
 // import BasicTodo from "./components/basicTodo/BasicTodo";
-import Todo from "./components/todo/Todo";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Todo from "./components/todo/Todo";
+import Home from "./components/RoutingPractice/Home";
+import About from "./components/RoutingPractice/About";
+import Contact from "./components/RoutingPractice/Contact";
+import NotFound from "./components/RoutingPractice/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Todo />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
